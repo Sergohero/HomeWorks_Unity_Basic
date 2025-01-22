f3f60cd6d10084930da4fbc712e6b200b402ae7d
Sergohero <ivaser1209@mail.ru>
2025-01-22 08:31:21 +0300

    Done math

---
+++ Assets/Scripts/HomeWork_2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeWork_2 : MonoBehaviour
+{
+    public int baseDamage = 0;
+    public float multiplier = 0f;
+    bool isTrue;
+
+    private void Start()
+    {
+        Debug.Log(baseDamage);
+        Debug.Log(multiplier);
+        Debug.Log(isTrue);
+        DealDamage();
+    }
+    public void DealDamage() 
+    {
+        int damage = 0;
+        damage = baseDamage * (int)multiplier;
+        Debug.Log(damage);
+    }
+}
