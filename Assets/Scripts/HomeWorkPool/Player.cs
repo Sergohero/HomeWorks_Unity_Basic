5a231dc27cbc94b2958561ef89e6af3f5a9191e8
Sergohero <ivaser1209@mail.ru>
2025-04-24 16:22:17 +0300

    add namespace

--- Assets/Scripts/HomeWorkPool/Player.cs
+++ Assets/Scripts/HomeWorkPool/Player.cs
@@ -1,46 +1,49 @@
-using System.Collections;
 using NTC.Pool;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace Scripts.HomeWorkPool
 {
-      [SerializeField] private GameObject _playerPrefab;
-      [SerializeField] private float _speed = 10f;
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private GameObject _playerPrefab;
+        [SerializeField] private float _speed = 10f;
 
       
-      private void Start()
-      {
+        private void Start()
+        {
             Instantiate(_playerPrefab, transform);
-      }
+        }
 
-      public void PlayerMovement()
-      {
+        public void PlayerMovement()
+        {
             if (Input.GetKey(KeyCode.W))
             {
-                  transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+                transform.Translate(Vector3.forward * _speed * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.S))
             {
-                  transform.Translate(Vector3.back * _speed * Time.deltaTime);
+                transform.Translate(Vector3.back * _speed * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.A))
             {
-                  transform.Translate(Vector3.left * _speed * Time.deltaTime);
+                transform.Translate(Vector3.left * _speed * Time.deltaTime);
             }
 
             if (Input.GetKey(KeyCode.D))
             {
-                  transform.Translate(Vector3.right * _speed * Time.deltaTime);
+                transform.Translate(Vector3.right * _speed * Time.deltaTime);
             }
-      }
+        }
 
-      public void OnTriggerEnter(Collider other)
-      {
+        public void OnTriggerEnter(Collider other)
+        {
             if (other.gameObject.CompareTag("Player"))
             {
-                  NightPool.Despawn(other.gameObject, 0.1f);
+                NightPool.Despawn(other.gameObject, 0.1f);
             }
-      }
+        }
+    }
 }
+
