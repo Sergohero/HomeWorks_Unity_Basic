d5e71d48aa548f41cc94c8f05c3470dfdcc3ab5f
Sergohero <ivaser1209@mail.ru>
2025-05-12 11:12:22 +0300

    Do AudioSystem

--- Assets/Scripts/HomeWork_4/MovementContriller.cs
+++ Assets/Scripts/HomeWork_4/MovementContriller.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace code
@@ -7,10 +8,19 @@ namespace code
         [SerializeField] private float _speed = 5.0f;
         [SerializeField] private Rigidbody _rb;
         [SerializeField] private Animator _animator;
+        [SerializeField] private AudioClip movementSound;
+        
+        private AudioSource audioSource; 
 
         private void Start()
         {
             _rb = GetComponent<Rigidbody>();
+            
+            if (audioSource == null)
+                audioSource = GetComponent<AudioSource>();
+            
+            if (audioSource == null)
+                audioSource = gameObject.AddComponent<AudioSource>();
         }
 
         private void Update()
@@ -20,6 +30,7 @@ namespace code
 
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
             Move(movement);
+            MovementSound();
         }
 
         private void Move(Vector3 direction)
@@ -28,6 +39,21 @@ namespace code
             _rb.velocity = new Vector3(velocity.x, 0, velocity.z);
 
             _animator.SetFloat("Speed", velocity.magnitude);
+        }
+
+        private void MovementSound()
+        {
+            float currentSpeed = _rb.velocity.magnitude;
+            if (currentSpeed > 0.1f && !audioSource.isPlaying && movementSound != null)
+            {
+                audioSource.clip = movementSound;
+                audioSource.loop = true;
+                audioSource.Play();
+            }
+            else if (currentSpeed <= 0.1f && audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
         }
     }
 }
--- Assets/Scripts/HomeWork_4/UseMedkit.cs
+++ Assets/Scripts/HomeWork_4/UseMedkit.cs
@@ -4,9 +4,22 @@ namespace code
 {
     public class UseMedkit : MonoBehaviour
     {
+        [SerializeField] private AudioClip[] _pickupSound;
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
-            Destroy(gameObject);
+            if (_pickupSound.Length > 0 && _audioSource != null)
+            {
+                AudioClip randomClip = _pickupSound[Random.Range(0, _pickupSound.Length)];
+                _audioSource.PlayOneShot(randomClip);
+                Destroy(gameObject, randomClip.length);
+            }
         }
     }
 }
